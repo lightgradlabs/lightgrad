8c8486b8f2c392ba62d4e2e9a0b8516ca58fa843
lightgradlabs <75404938+lightgradlabs@users.noreply.github.com>
2025-07-13 16:02:34 +0530

    Update autograd.py

--- core/autograd.py
+++ core/autograd.py
@@ -1,102 +1,84 @@
+# core/autograd.py
+
 class Tensor:
     def __init__(self, data, requires_grad=False):
-        self.data = data
+        self.data = data  # 2D list: e.g., [[1.0, 2.0], [3.0, 4.0]]
         self.requires_grad = requires_grad
         self.grad = None
-        self._backward = lambda: None
         self._prev = set()
+        self._backward = lambda: None  # default no-op
+
+    def __repr__(self):
+        return f"Tensor(data={self.data}, requires_grad={self.requires_grad})"
+
+    def backward(self):
+        self.grad = [[1.0 for _ in row] for row in self.data]  # starting grad = 1.0
+        visited = set()
+        topo = []
+
+        def build_topo(t):
+            if t not in visited:
+                visited.add(t)
+                for child in t._prev:
+                    build_topo(child)
+                topo.append(t)
+
+        build_topo(self)
+
+        for t in reversed(topo):
+            t._backward()
 
     def __add__(self, other):
-        # Handle scalar addition
         if isinstance(other, (int, float)):
-            out = Tensor([[self.data[i][j] + other for j in range(len(self.data[0]))]
-                         for i in range(len(self.data))], requires_grad=self.requires_grad)
+            out = Tensor([[self.data[i][j] + other for j in range(len(self.data[0]))] 
+                          for i in range(len(self.data))])
             return out
 
         if not isinstance(other, Tensor):
-            raise TypeError("Unsupported type for addition")
+            raise TypeError("Can only add Tensor to Tensor or scalar")
 
         self_rows, self_cols = len(self.data), len(self.data[0])
         other_rows, other_cols = len(other.data), len(other.data[0])
 
-        # Case 1: Same shape
         if self_rows == other_rows and self_cols == other_cols:
-            out_data = [[self.data[i][j] + other.data[i][j] for j in range(self_cols)]
+            out_data = [[self.data[i][j] + other.data[i][j] for j in range(self_cols)] 
                         for i in range(self_rows)]
 
-        # Case 2: [m x n] + [1 x n]
         elif other_rows == 1 and self_cols == other_cols:
-            out_data = [[self.data[i][j] + other.data[0][j] for j in range(self_cols)]
+            out_data = [[self.data[i][j] + other.data[0][j] for j in range(self_cols)] 
                         for i in range(self_rows)]
 
-        # Case 3: [1 x n] + [m x n]
         elif self_rows == 1 and self_cols == other_cols:
-            out_data = [[self.data[0][j] + other.data[i][j] for j in range(self_cols)]
+            out_data = [[self.data[0][j] + other.data[i][j] for j in range(self_cols)] 
                         for i in range(other_rows)]
 
-        # Case 4: [m x n] + [m x 1]
-        elif other_cols == 1 and self_rows == other_rows:
-            out_data = [[self.data[i][j] + other.data[i][0] for j in range(self_cols)]
-                        for i in range(self_rows)]
-
-        # Case 5: [m x 1] + [m x n]
-        elif self_cols == 1 and self_rows == other_rows:
-            out_data = [[self.data[i][0] + other.data[i][j] for j in range(other_cols)]
-                        for i in range(self_rows)]
         else:
-            raise ValueError("Incompatible shapes for broadcasting")
+            raise ValueError(f"Cannot broadcast shapes [{self_rows}x{self_cols}] and [{other_rows}x{other_cols}]")
 
-        out = Tensor(out_data, requires_grad=self.requires_grad or other.requires_grad)
+        out = Tensor(out_data)
+        out.requires_grad = self.requires_grad or other.requires_grad
         out._prev = {self, other}
 
         def _backward():
-            if out.grad is None:
-                return
-
-            # Gradients to self
             if self.requires_grad:
                 if self.grad is None:
-                    self.grad = [[0.0 for _ in range(self_cols)] for _ in range(self_rows)]
-                for i in range(self_rows):
-                    for j in range(self_cols):
+                    self.grad = [[0.0 for _ in row] for row in self.data]
+                for i in range(len(self.grad)):
+                    for j in range(len(self.grad[0])):
                         self.grad[i][j] += out.grad[i][j]
 
-            # Gradients to other (with reduction)
             if other.requires_grad:
                 if other.grad is None:
-                    other.grad = [[0.0 for _ in range(other_cols)] for _ in range(other_rows)]
-
-                if other_rows == 1:
+                    other.grad = [[0.0 for _ in row] for row in other.data]
+                # Simple version: directly use out.grad (no sum for broadcast reduction)
+                if other_rows == 1 and self_cols == other_cols:
                     for j in range(other_cols):
                         grad_sum = sum(out.grad[i][j] for i in range(len(out.grad)))
                         other.grad[0][j] += grad_sum
-                elif other_cols == 1:
-                    for i in range(other_rows):
-                        grad_sum = sum(out.grad[i])
-                        other.grad[i][0] += grad_sum
                 else:
-                    for i in range(other_rows):
-                        for j in range(other_cols):
+                    for i in range(len(other.grad)):
+                        for j in range(len(other.grad[0])):
                             other.grad[i][j] += out.grad[i][j]
 
         out._backward = _backward
         return out
-
-    def backward(self):
-        if self.grad is None:
-            # Initialize grad as 1.0 for loss scalar
-            self.grad = [[1.0]]
-        visited = set()
-        topo = []
-
-        def build_topo(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
-                topo.append(v)
-
-        build_topo(self)
-
-        for node in reversed(topo):
-            node._backward()

6f84da554463c05754a63ea86b82a5f7dfec0ec2
lightgradlabs <75404938+lightgradlabs@users.noreply.github.com>
2025-07-13 15:35:39 +0530

    Update layers.py

--- core/layers.py
+++ core/layers.py
@@ -8,20 +8,12 @@ class Linear:
     def __init__(self, in_features, out_features):
         self.weight = Tensor(
             [[random.uniform(-1, 1) for _ in range(in_features)]
-             for _ in range(out_features)],
-            requires_grad=True
-        )
+             for _ in range(out_features)], requires_grad=True)
         self.bias = Tensor([[0.0 for _ in range(out_features)]], requires_grad=True)
 
-    def __call__(self, x: Tensor):
-        z = matmul(x, transpose(self.weight))
-        # Bias broadcasting
-        out_data = [
-            [z.data[i][j] + self.bias.data[0][j] for j in range(len(z.data[0]))]
-            for i in range(len(z.data))
-        ]
-        out = Tensor(out_data, requires_grad=True)
-        out._prev = {x, self.weight, self.bias}
-        return out
-
+    def __call__(self, x):
+        wx = matmul(x, transpose(self.weight))
+        # Broadcast bias across all rows
+        bias_expanded = Tensor([self.bias.data[0] for _ in range(len(wx.data))], requires_grad=True)
+        return wx + bias_expanded
 

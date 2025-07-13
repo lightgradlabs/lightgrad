b15117b9e9270415c62b2309b28c8247094de939
lightgradlabs <75404938+lightgradlabs@users.noreply.github.com>
2025-07-13 15:35:15 +0530

    Update autograd.py

--- core/autograd.py
+++ core/autograd.py
@@ -9,25 +9,14 @@ class Tensor:
         self._prev = set()
 
     def __add__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor([[other]])
-        out_data = []
-        for i in range(len(self.data)):
-            row = []
-            for j in range(len(self.data[0])):
-                # Support broadcasting for bias shapes
-                if len(other.data) == 1:
-                    if len(other.data[0]) == 1:
-                        val = other.data[0][0]  # scalar bias
-                    else:
-                        val = other.data[0][j]  # row vector bias
-                else:
-                    val = other.data[i][j]
-                row.append(self.data[i][j] + val)
-            out_data.append(row)
+        assert len(self.data) == len(other.data)
+        assert len(self.data[0]) == len(other.data[0])
 
+        out_data = [
+            [self.data[i][j] + other.data[i][j] for j in range(len(self.data[0]))]
+            for i in range(len(self.data))
+        ]
         out = Tensor(out_data, requires_grad=self.requires_grad or other.requires_grad)
-        out._prev = {self, other}
 
         def _backward():
             if self.requires_grad:
@@ -38,27 +27,23 @@ class Tensor:
                 for i in range(len(other.grad)):
                     for j in range(len(other.grad[0])):
                         other.grad[i][j] += out.grad[i][j]
-
         out._backward = _backward
+        out._prev = {self, other}
         return out
 
     def backward(self):
-        if self.grad is None:
-            self.grad = [[1.0 for _ in row] for row in self.data]
-
-        visited = set()
+        self.grad = [[1.0 for _ in row] for row in self.data]
         topo = []
+        visited = set()
 
-        def build_topo(tensor):
-            if tensor not in visited:
-                visited.add(tensor)
-                for parent in tensor._prev:
-                    build_topo(parent)
-                topo.append(tensor)
+        def build_topo(t):
+            if t not in visited:
+                visited.add(t)
+                for child in t._prev:
+                    build_topo(child)
+                topo.append(t)
 
         build_topo(self)
-
         for t in reversed(topo):
             t._backward()
-
 

27cf33b9e80c09f01800691d8142df1c35b7ce6f
lightgradlabs <75404938+lightgradlabs@users.noreply.github.com>
2025-07-13 15:17:38 +0530

    Update train_xor_final.py

--- train_xor_final.py
+++ train_xor_final.py
@@ -1,34 +1,37 @@
-# XOR training using LightGrad full stack (autograd + matmul)
-
 from core.autograd import Tensor
 from core.loss import mse_loss
-from core.layers import Linear
-from core.tensor_ops import add  # for bias
+from core.matmul import matmul, transpose
 import random
 
-# XOR data
+class Linear:
+    def __init__(self, in_features, out_features):
+        self.weight = Tensor([[random.uniform(-1, 1) for _ in range(in_features)]
+                              for _ in range(out_features)], requires_grad=True)
+        self.bias = Tensor([[0.0 for _ in range(out_features)]], requires_grad=True)
+
+    def __call__(self, x: Tensor):
+        return matmul(x, transpose(self.weight)) + self.bias
+
+def relu(t: Tensor):
+    out_data = [[max(0, val) for val in row] for row in t.data]
+    out = Tensor(out_data, requires_grad=t.requires_grad)
+    def _backward():
+        if t.requires_grad:
+            t.grad = [[1 if val > 0 else 0 for val in row] for row in t.data]
+    out._backward = _backward
+    out._prev = {t}
+    return out
+
+# XOR dataset
 x_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
 y_data = [[0], [1], [1], [0]]
-
-x = Tensor(x_data, requires_grad=False)
-y = Tensor(y_data, requires_grad=False)
+x = Tensor(x_data)
+y = Tensor(y_data)
 
 # Model
 l1 = Linear(2, 4)
 l2 = Linear(4, 1)
 lr = 0.1
-
-def relu(t: Tensor):
-    relu_data = [[max(0, val) for val in row] for row in t.data]
-    out = Tensor(relu_data, requires_grad=t.requires_grad)
-    out._prev = {t}
-
-    def _backward():
-        if t.requires_grad:
-            t.grad = [[1 if val > 0 else 0 for val in row] for row in t.data]
-
-    out._backward = _backward
-    return out
 
 # Training loop
 for epoch in range(50):
@@ -38,30 +41,21 @@ for epoch in range(50):
     pred = z2
 
     loss = mse_loss(pred, y)
-    loss.backward()                                                                                       
-    # Safe fallback in case gradients aren't set
-if l2.weight.grad is None:
-    l2.weight.grad = [[0 for _ in row] for row in l2.weight.data]
-if l2.bias.grad is None:
-    l2.bias.grad = [[0 for _ in row] for row in l2.bias.data]
-if l1.weight.grad is None:
-    l1.weight.grad = [[0 for _ in row] for row in l1.weight.data]
-if l1.bias.grad is None:
-    l1.bias.grad = [[0 for _ in row] for row in l1.bias.data]
+    loss.backward()
 
-
-    # Gradient descent
+    # Manual update
     for i in range(len(l2.weight.data)):
         for j in range(len(l2.weight.data[0])):
-            l2.weight.data[i][j] -= lr * l2.weight.grad[i][j]
+            l2.weight.data[i][j] -= lr * (l2.weight.grad[i][j] if l2.weight.grad else 0)
     for j in range(len(l2.bias.data[0])):
-        l2.bias.data[0][j] -= lr * l2.bias.grad[0][j]
+        l2.bias.data[0][j] -= lr * (l2.bias.grad[0][j] if l2.bias.grad else 0)
 
     for i in range(len(l1.weight.data)):
         for j in range(len(l1.weight.data[0])):
-            l1.weight.data[i][j] -= lr * l1.weight.grad[i][j]
+            l1.weight.data[i][j] -= lr * (l1.weight.grad[i][j] if l1.weight.grad else 0)
     for j in range(len(l1.bias.data[0])):
-        l1.bias.data[0][j] -= lr * l1.bias.grad[0][j]
+        l1.bias.data[0][j] -= lr * (l1.bias.grad[0][j] if l1.bias.grad else 0)
 
     if epoch % 5 == 0:
         print(f"Epoch {epoch+1}: Loss = {round(loss.data[0][0], 4)}")
+

55bf835337b0255ab391109c1c8a30070ac914db
lightgradlabs <75404938+lightgradlabs@users.noreply.github.com>
2025-07-13 15:58:08 +0530

    Update layers.py

--- core/layers.py
+++ core/layers.py
@@ -1,23 +1,22 @@
-# core/layers.py
-
 import random
 from core.autograd import Tensor
 from core.matmul import matmul, transpose
 
 class Linear:
     def __init__(self, in_features, out_features):
-        import random
-        self.weight = Tensor(
-            [[random.uniform(-1, 1) for _ in range(in_features)]
-             for _ in range(out_features)], requires_grad=True)
-        self.bias = Tensor([[0.0 for _ in range(out_features)]], requires_grad=True)
-
+        self.in_features = in_features
+        self.out_features = out_features
+        
+        self.weight = Tensor([[random.uniform(-0.5, 0.5) for _ in range(in_features)] 
+                             for _ in range(out_features)])
+        self.weight.requires_grad = True
+        
+        self.bias = Tensor([[random.uniform(-0.5, 0.5) for _ in range(out_features)]])
+        self.bias.requires_grad = True
+    
     def __call__(self, x):
-        out = matmul(x, transpose(self.weight))  # Shape: [batch_size x out_features]
-
-        # ✅ Broadcast bias manually
-        bias_expanded = Tensor([self.bias.data[0] for _ in range(len(out.data))], requires_grad=True)
-
-        return out + bias_expanded
-
+        return matmul(x, transpose(self.weight)) + self.bias
+    
+    def parameters(self):
+        return [self.weight, self.bias]
 
